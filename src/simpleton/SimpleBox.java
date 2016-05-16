1f4ceff073b8bb9dd06b053296b11d208ca3de7d
Ciarán Treacy <ciaran.treacy@version1.com>
2016-05-16 21:07:34 +0100

    Second SimpleObject added to main

--- src/simpleton/SimpleBox.java
+++ src/simpleton/SimpleBox.java
@@ -5,6 +5,8 @@ public class SimpleBox {
 	public static void main(String[] args) {
 		SimpleObject so = new SimpleObject();
 		System.out.println(so);
+		SimpleObject so2 = new SimpleObject("Cosmo", 16);
+		System.out.println(so2);
 
 	}
 
